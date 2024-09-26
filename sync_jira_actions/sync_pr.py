35b6cb3af30ea360ab476019c8d6c8b2b5149cfc
samitab <samitab@cisco.com>
2024-09-27 00:50:34 +1000

    feat: Approved PRs can transition thier linked Jira issues to a Read to merge status

--- sync_jira_actions/sync_pr.py
+++ sync_jira_actions/sync_pr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #
 # Copyright 2019-2024 Espressif Systems (Shanghai) CO LTD
+# Copyright: (c) 2024, Samita Bhattacharjee (@samiib) <samitab@cisco.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,12 +17,14 @@
 #
 import os
 import re
-import requests
-
+from jira import JIRA
 from github import Github
 from sync_issue import _create_jira_issue
 from sync_issue import _find_jira_issue
+from github_graphql import find_closing_issues, get_pr_review_status
 
+# The minimum number of approvals before a PR is ready to merge.
+MINIMUM_APPROVALS = int(os.environ.get('INPUT_MINIMUM_APPROVALS', 3))
 
 def sync_remain_prs(jira):
     """
@@ -59,7 +62,7 @@ def find_and_link_pr_issues(gh_issue):
     repo = github.get_repo(os.environ['GITHUB_REPOSITORY'])
     pr_number = int(gh_issue['number'])
     pr_title = gh_issue['title']
-    closing_issues = __find_closing_issues(token, repo.owner.login, repo.name, pr_number)
+    closing_issues = find_closing_issues(token, repo.owner.login, repo.name, pr_number)
     jira_keys = []
     for issue in closing_issues:
         title = issue.get('title')
@@ -74,30 +77,35 @@ def find_and_link_pr_issues(gh_issue):
         new_pr_title = f'{new_pr_title} ({" ".join(jira_keys)})'
         print(f'New PR title: {new_pr_title}')
         repo.get_issue(pr_number).edit(title=new_pr_title)
-        return True
-    return False
+    return jira_keys
 
+def check_pr_approval_and_move(jira: JIRA, gh_issue, jira_keys):
+    """
+    Checks the approval criteria of the PR and moves any linked Jira issues to Approved or back to review.
+    """
+    token = os.environ['GITHUB_TOKEN']
+    github = Github(os.environ['GITHUB_TOKEN'])
+    repo = github.get_repo(os.environ['GITHUB_REPOSITORY'])
+    pr_number = int(gh_issue['number'])
+    approved = __check_pr_approval_status(pr_number, repo, token)
+    for key in jira_keys:
+        issue_status = str(jira.issue(key).get_field("status"))
+        print(f"{key}: status '{issue_status}' approved '{approved}'")
+        if approved and issue_status == "Review in progress":
+            print(f"{key}: Transition to Approved")
+            jira.transition_issue(key, "Approved")
+            jira.add_comment(key, "The PR linked to this issue has met approval criteria and is ready to merge.")
+        if not approved and issue_status == "Reviewer Approved":
+            print(f"{key}: Transition back to review in progress")
+            jira.transition_issue(key, "Requires re-review")
+            jira.add_comment(key, "The PR linked to this issue has new changes requested and moved back to review.")
 
-def __find_closing_issues(token, owner, repo, pr):
-    headers = {"Authorization": f"Bearer {token}"}
-    vars = {"owner": owner, "repo": repo, "pr": pr}
-    query = """
-        query($owner:String!, $repo:String!, $pr:Int!) {
-            repository (owner: $owner, name: $repo) {
-                pullRequest (number: $pr) {
-                    closingIssuesReferences (first: 10) {
-                        nodes {
-                            number
-                            title
-                        }
-                    }
-                }
-            }
-        }
-        """
-    request = requests.post('https://api.github.com/graphql', json={'query': query, 'variables': vars}, headers=headers)
-    if request.status_code == 200:
-        closing_issues = request.json().get('data').get('repository').get('pullRequest').get('closingIssuesReferences').get('nodes')
-        return closing_issues
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+def __check_pr_approval_status(pr, repo, token):
+    """
+    Checks the approval criteria of the PR
+    """
+    status = get_pr_review_status(token, repo.owner.login, repo.name, pr)
+    approved = status.outcome == "APPROVED"
+    num_approved = len([r for r in status.reviews if r == "APPROVED"])
+    print(f"PR Status: {status.outcome} with {num_approved}/{MINIMUM_APPROVALS} approvals")
+    return approved and num_approved >= MINIMUM_APPROVALS
